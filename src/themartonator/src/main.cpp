bc8a7535f140626ba4088370368a0be7f7bf7ec1
pgergq <pgergq@gmail.com>
2019-01-15 00:35:18 +1300

    extend baseline cmake config with hello world code

--- src/themartonator/src/main.cpp
+++ src/themartonator/src/main.cpp
@@ -1,7 +1,16 @@
+#include <boost/any.hpp>
+
+#include <array>
 #include <iostream>
+
 
 int main()
 {
+    auto arr = std::array{boost::any{0}, boost::any{1}, boost::any{2}};
     std::cout << "hello world" << std::endl;
+    for (auto&& e : arr)
+    {
+        std::cout << e.type().name() << std::endl;
+    }
     return 0;
 }
