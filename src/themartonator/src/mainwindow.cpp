1c48a97f5ffe4f677621f388663f185faf3ef6ee
pgergq <pgergq@gmail.com>
2019-01-22 21:48:26 +1300

    tweak main window layout

--- src/themartonator/src/mainwindow.cpp
+++ src/themartonator/src/mainwindow.cpp
@@ -7,9 +7,7 @@
 
 using namespace martonator;
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 }
